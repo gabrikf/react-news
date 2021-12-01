afa3904915f1d25fc4676f969f57cdc283f7c258
Gabriel Koch Fodi <gabrielkochf@gmail.com>
2021-12-01 12:40:29 -0300

    serversiderendering class

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,8 @@
 import styles from "./styles.module.scss";
-
-export function SubscribeButton() {
+interface SubscibeButtonProps {
+  priceId: string;
+}
+export function SubscribeButton({priceId}: SubscibeButtonProps) {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,17 @@ import Head from "next/head";
 import { GetServerSideProps } from 'next' 
 import { SubscribeButton } from "../components/SubscribeButton";
 import styles from './home.module.scss'
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product : {
+    priceId: string;
+    amount: number;
+
+  }
+}
+
+export default function Home({product}:HomeProps) {
   return (
     <>
       <Head>
@@ -16,9 +25,9 @@ export default function Home() {
         <h1>News about the <span>React</span> world.</h1>
         <p>
           Get access to all the publications<br />
-          <span>for $9.90 month</span>
+          <span>for {product.amount} month</span>
         </p>
-        <SubscribeButton />
+        <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
@@ -29,9 +38,23 @@ export default function Home() {
 
 // everething that a use here will hsappen in snode server, not in browser 
 export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1K0tHmGCQHtsbFUAjY3gGqvn',{
+    expand:['product'] // get all product's info
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency:'USD'
+    }).format((price.unit_amount / 100)),
+    
+    
+
+  }
   return {
      props:{
-
+      product
      }
   }
 }
